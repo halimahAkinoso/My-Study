30b48d82473019dc95fbbf286882a6da990466e3
Halimah Akinoso <halimahAkin2@gmail.com>
2026-07-16 17:01:28 +0100

    vite APi fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
